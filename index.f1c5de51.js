6314597d30417c1e6818f832e3e55857a408f35c
MishaHryvinskyi <116170668+MishaHryvinskyi@users.noreply.github.com>
2025-03-24 11:36:43 +0000

    Deploying to gh-pages from @ MishaHryvinskyi/barbershop@b4b44490daf8048565f62f8e91f333b23f8350b6 🚀

---
+++ index.f1c5de51.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".menu-js"),n=document.querySelector(".btn-open-js"),t=document.querySelector(".btn-close-js");n.addEventListener("click",(function(){e.classList.add("open")})),t.addEventListener("click",(function(){e.classList.remove("open")}))}();
+//# sourceMappingURL=index.f1c5de51.js.map
