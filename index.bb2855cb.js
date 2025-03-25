f74c9aa72eb08730804632fc8a8af364dfc95298
MishaHryvinskyi <116170668+MishaHryvinskyi@users.noreply.github.com>
2025-03-25 17:21:36 +0000

    Deploying to gh-pages from @ MishaHryvinskyi/barbershop@c56850035b715b8cde59c95d2a5809fd1a519f29 🚀

--- index.bb2855cb.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".menu-js"),n=document.querySelector(".btn-open-js"),t=document.querySelector(".btn-close-js");n.addEventListener("click",(function(){e.classList.add("open")})),t.addEventListener("click",(function(){e.classList.remove("open")}));
-//# sourceMappingURL=index.bb2855cb.js.map
